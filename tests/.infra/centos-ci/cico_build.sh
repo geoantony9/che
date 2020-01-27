f2bd0f6a652e839f5b56bb615e74ca1ffef87699
musienko maksym <mmusiien@redhat.com>
2020-01-27 17:27:17 +0200

    remove unnecessary file set job as failded if the test's not passed

--- tests/.infra/centos-ci/cico_build.sh
+++ tests/.infra/centos-ci/cico_build.sh
@@ -6,31 +6,26 @@
 # http://www.eclipse.org/legal/epl-v10.html
 set +e
 
-
 echo "========Starting nigtly test job $(date)========"
-
-
-
-total_start_time=$(date +%s)
 
 source tests/.infra/centos-ci/functional_tests_utils.sh
 source .ci/cico_common.sh
 
-setupEnvs
-checkAllCreds
-load_mvn_settings_gpg_key
-installDependencies
-buildAndDeployArtifacts
-testImages "nightly"
+installKVM
+installAndStartMinishift
+loginToOpenshiftAndSetDevRople
+installCheCtl
+deployCheIntoCluster
+createTestUserAndObtainUserToken
+if createTestWorkspaceAndRunTest; then
+    echo '========Tests passed successfully========'
+    archiveArtifacts
+    echo "========Finishing nigtly test job $(date)========"
+else
+    archiveArtifacts
+    echo "========Finishing nigtly test job $(date)========"
+    set -x
+    exit 1
+fi
 
 
-
-#installKVM
-#installAndStartMinishift
-#loginToOpenshiftAndSetDevRople
-#installCheCtl
-#deployCheIntoCluster
-#createTestUserAndObtainUserToken
-#createTestWorkspaceAndRunTest
-#archiveArtifacts
-echo "========Starting nigtly test job $(date)========"

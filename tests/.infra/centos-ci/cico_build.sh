e1977d65bb9ddfac345ae5f99f7dcf82fd1d18b8
musienko maksym <mmusiien@redhat.com>
2020-01-28 10:04:12 +0200

    set exit immediately after err.

--- tests/.infra/centos-ci/cico_build.sh
+++ tests/.infra/centos-ci/cico_build.sh
@@ -4,7 +4,7 @@
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-set +e
+set -e
 
 echo "========Starting nigtly test job $(date)========"
 
@@ -18,15 +18,6 @@ installAndStartMinishift
 loginToOpenshiftAndSetDevRople
 deployCheIntoCluster
 createTestUserAndObtainUserToken
-if createTestWorkspaceAndRunTest; then
-    echo '========Tests passed successfully========'
-    archiveArtifacts
-    echo "========Finishing nigtly test job $(date)========"
-else
-    archiveArtifacts
-    echo "========Finishing nigtly test job $(date)========"
-    set -x
-    exit 1
-fi
-
+createTestWorkspaceAndRunTest
+archiveArtifacts
 
